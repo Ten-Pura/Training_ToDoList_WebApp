5b99885f3a74c19e09a95e395e0ef3760283616c
Ten-Pura <88696456+Ten-Pura@users.noreply.github.com>
2025-03-09 04:29:52 +0000

    SQLAlchemyをインストール

--- use_form_post/app.py
+++ use_form_post/app.py
@@ -1,22 +1,19 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
-#掲示板の一つ一つのメッセージを示すクラス
-class Message:
-    #コンストラクター
-    def __init__(self, id: str, user_name: str, contents: str):
-        self.id = id
-        self.user_name = user_name
-        self.contents = contents
-
-#グローバル変数の宣言
 app: Flask = Flask(__name__)
 login_user_name: str = "osamu"
-message_list: list[Message] = [
-        Message("202400502102310", "osamu", "朝からビールですか！楽しみです。"),
-        Message("202400502100223", "noriko", "こちらこそ！次回はABコースで！"),
-        Message("202400502092101", "osamu", "昨日はHBコーズ楽しかったです！")
-    ]
+
+#Databaseの設定
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
+
+#メッセージのデータベースモデル
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(100))
+    contents = db.Column(db.String(100))
 
 @app.route("/")
 def index():
@@ -25,17 +22,15 @@ def index():
 
     #search_word変数の有無を判定
     if search_word is None:
-        return render_template("top.html", login_user_name=login_user_name, message_list=message_list)
+        message_list: list[Message] = Message.query.all()
     else:
-        filtered_message_list: list[Message] = [
-            message for message in message_list if search_word in message.contents
-        ]
-        return render_template(
-            "top.html",
-            login_user_name=login_user_name, 
-            message_list=filtered_message_list,
-            search_word=search_word
-            )
+        message_list: list[Message] = Message.query.fillter(Message.contents.like(f"%{search_word}%")).all()
+    return render_template(
+        "top.html",
+        login_user_name=login_user_name, 
+        message_list=message_list,
+        search_word=search_word
+        )
 
 @app.route("/write", methods=["GET", "POST"])
 def write():
@@ -44,13 +39,18 @@ def write():
     elif request.method == "POST":
         #POSTメソッドのフォーム値を利用して
         #新しいMessageクラスインスタンスを生成する
-        id: str = datetime.now().strftime("%Y%m%d%H%M%S")
         contents: str = request.form.get("contents")
         user_name: str = request.form.get("user_name")
+        new_message = Message(user_name=user_name, contents=contents)
+        db.session.add(new_message)
+        #変更をデータベースにコミット
+        db.session.commit()
         #message_listに追加してtop.htmlに表示
-        if contents:
-            message_list.insert(0, Message(id, user_name, contents))
-        return render_template("top.html", login_user_name=login_user_name, message_list=message_list)
-
+        return redirect(url_for("index"))
+    
+#データベースの初期化
+with app.app_context():
+    db.create_all()
+    
 if __name__ == "__main__":
     app.run(debug=True)
