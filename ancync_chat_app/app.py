dbb56b228b91f5654c8d57ce8958e0fb0a70ec70
Ten-Pura <88696456+Ten-Pura@users.noreply.github.com>
2025-03-11 08:25:44 +0000

    途中経過

---
+++ ancync_chat_app/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from datetime import datetime
+from pymongo import MongoClient
+
+app: Flask = Flask(__name__)
+
+#Socket.ioのセットアップ
+socketio = SocketIO(app)
+
+#MongoDBの接続設定
+mongo_uri = "mongodb+srv://taichi1166:<db_password>@cluster0.jslmr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(mongo_uri)
+db = client["SNS"]
+messages_collection = db["messages"]
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@socketio.on('load messages')
+def load_messages():
+    messages = messages_collection.find().sort("_id", -1).limit(10)
+    print("動いている_その1")
+    messages = list(messages)[::-1]
+    print(len(messages))
+    #messages_return = [message['message'] for message in messages]
+    #print("動いている＿その1")
+    #メッセージをクライアントへ送信
+    #emit('load all messages', messages_return)
+
+
+@socketio.on('send message')
+def send_message(message):
+    print("Send＿Message動いているその1")
+    messages_collection.insert_one({'message': message})
+    print("動いている＿その2")
+    #メッセージをクライアントへ送信
+    emit('load one message', message, broadcast=True)
+
+if __name__ == "__main__":
+    #socket.ioサーバーの起動
+    socketio.run(app, debug=True)
