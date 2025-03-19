f7643e7ad687d443ea27af372619f1de5c43f555
Ten-Pura <taichi1166@gmail.com>
2025-03-19 22:45:44 +0900

    途中経過

--- cha4_reversi_app/reversi_util.py
+++ cha4_reversi_app/reversi_util.py
@@ -50,14 +50,59 @@ def can_flip(board:list[list[int]], x:int, y:int, who:int) -> bool:
     return False
 
 #方向(dx,dy)に石を置く
-def flip_dir(board:list[list[int]], x:int, y:int, dx:int, dy:int, who:int) -> bool:
+def flip_dir(board:list[list[int]], x:int, y:int, dx:int, dy:int, who:int) -> int:
     if not can_flip_dir(board, x, y, dx, dy, who):
         return 0
     count = 0
     for i in range(1, 8):
-        if not is_on_board
+        if not is_on_board(x + dx*i, y + dy*i):
+            break
+        if board[y + dy*i][x + dx*i] == who:
+            break
+        board[y + dy*i][x + dx*i] = who
+        count += 1
+    return count
+
+#位置(x,y)に石を置く。ひっくり返した石の数を返す
+def flip(board: list[list[int]], x:int, y:int, who:int) -> int:
+    if not can_flip(board, x, y, who):
+        return 0
+    count = 0
+    for dx, dy in DIR_OFFSET:
+        count += flip_dir(board, x, y, dx, dy, who)
+    board[y][x] = who
+    return count
+
+#石を置ける場所にマークを付ける関数
+def add_flip_mark(board: list[list[int]], who:int) -> list[list[int]]:
+    res = generate_board()
+    for y in range(8):
+        for x in range(8):
+            res[y][x] = board[y][x]
+            if board[y][x] == EMPTY:
+                if can_flip(board, x, y, who):
+                    res[y][x] = CAN_FLIP
+    return res
+
+#石の数を数える
+def count_stone(board: list[list[int]], who:int ) -> int:
+    return sum([ row.count(who) for row in board ])
+def count_stone_both(board: list[list[int]]) -> tuple[int, int]:
+    return count_stone(board, BLACK), count_stone(board, WHITE)
 
 if __name__ == "__main__":
     l = generate_board()
+    l = add_flip_mark(l, 2)
+    m = [[" "*8] for _ in range(8)]
+    for y in range(6):
+        for x in range(6):
+            if    l[y][x] == 1: m[y][x] = "●"
+            elif  l[y][x] == 2: m[y][x] = "○"
+            else: m[y][x] = "□"
     for i in l:
         print(i)
+    for i in m:
+        print(i)
+    count = count_stone_both(l)
+    print("Black_stone:", count[0])
+    print("White_stone:", count[1])
